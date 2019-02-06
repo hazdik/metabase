76a8ddba2d33201e978d169b183cb936ae745a1b
Tom Robinson <tlrobinson@gmail.com>
2019-02-05 16:35:58 -0800

    Add EditUserModal.integ.spec.js and integration test helpers

---
+++ frontend/test/metabase/admin/people/containers/EditUserModal.integ.spec.js
@@ -0,0 +1,26 @@
+import React from "react";
+import mock from "xhr-mock";
+import { mountWithStore } from "__support__/integration_tests";
+
+import EditUserModal from "metabase/admin/people/containers/EditUserModal";
+
+const MOCK_USER = {
+  id: 42,
+  first_name: "Test",
+  last_name: "Test",
+  email: "test@metabase.com",
+};
+
+describe("EditUserModal", () => {
+  beforeEach(() => mock.setup());
+  afterEach(() => mock.teardown());
+
+  it("should load the user", async () => {
+    mock.get("/api/user/42", (req, res) => res.json(MOCK_USER));
+    const { wrapper, store } = mountWithStore(
+      <EditUserModal params={{ userId: 42 }} />,
+    );
+    await store.waitForAction("metabase/requests/SET_REQUEST_STATE");
+    expect(wrapper.find(".Icon-close").exists()).toBe(true);
+  });
+});
